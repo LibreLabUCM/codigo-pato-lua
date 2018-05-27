2b2b1964eac17ff998be17e2046330840b825e3b
Jorge Blázquez Saborido <jorge.jbs@protonmail.com>
2018-05-27 11:14:05 +0200

    Load lua library directly from target directory

--- test.lua
+++ test.lua
@@ -1,4 +1,3 @@
--- `codp.so` has to be in the same directory as this file, so you have to copy
--- it from `target` to run this test.
+package.cpath = package.cpath .. ";./target/?.so"
 local codp = require("codp")
 print(codp.sin(3.1415/2))
